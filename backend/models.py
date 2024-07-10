a0286826874eb39011a8b2a56700d9f52886d131
Kunal Mansukhani <kmmansukhani@gmail.com>
2024-07-10 01:50:21 -0400

    added s3 upload

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,4 @@
 from pydantic import BaseModel
-from typing import Optional
 
 class TextInput(BaseModel):
     text: str
-    style: Optional[str] = "default"
-    use_claude: Optional[bool] = False
--- backend/routers/video.py
+++ backend/routers/video.py
@@ -1,29 +1,35 @@
+from pathlib import Path
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 from backend.models import TextInput
 from backend.services.generate_manim_code import generate_manim_code
 from backend.services.render_manim_video import render_manim_video
+import boto3
 
 router = APIRouter()
+s3 = boto3.client('s3', 
+                  aws_access_key_id="AKIA2UC3EIU4QANEQKGD",
+                  aws_secret_access_key="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")
+
+BUCKET_NAME = 'an-gen'
 
 @router.post("/generate_video")
 async def generate_video(input: TextInput):
     try:
         manim_code = generate_manim_code(input.text)
         video_path = render_manim_video(manim_code)
-        video_url = f"http://localhost:8000/{str(video_path).replace('backend/', '', 1)}"
-
-        return JSONResponse(content={"videoUrl": video_url})
+        s3_video_url = upload_video_to_s3(video_path)
+        print(f"Video URL: {s3_video_url}")
+        return JSONResponse(content={"videoUrl": s3_video_url})
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail=str(e))
     
-# This is purely for RAG testing purposes
-@router.post("/generate_code")
-async def generate_code(input: TextInput):
+def upload_video_to_s3(video_path: Path) -> str:
     try:
-        manim_code = generate_manim_code(input.text, input.style, True)
-        return JSONResponse(content={"code": manim_code})
+        s3.upload_file(str(video_path), BUCKET_NAME, video_path.name)
+        s3_video_url = f"https://{BUCKET_NAME}.s3.amazonaws.com/{video_path.name}"
+        return s3_video_url
     except Exception as e:
-        print(e)
-        raise HTTPException(status_code=500, detail=str(e))
+        print(f"Error uploading video to S3: {str(e)}")
+        raise HTTPException(status_code=500, detail="Failed to upload video to S3")
